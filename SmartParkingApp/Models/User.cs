92ed693638033e4829dc2a8a9e80ceb623f310f9
Павел Петров <pvpetrov@edu.hse.ru>
2019-11-20 10:00:00 +0300

    v1

---
+++ SmartParkingApp/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartParkingApp.Models {
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CarPlateNumber { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ SmartParkingApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmartParkingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smart parking application");
+
+            var manager = new ParkingManager();
+
+            // Sample code goes here           
+        }
+    }
+}
